7ae699d11d4181ea58d34bc736adae335a20256c
Camila Garcia <garciacamilalorena@gmail.com>
2023-07-11 11:40:10 -0300

    Modifications to correct the seasons in exercise

--- section6_47.py
+++ section6_47.py
@@ -1,10 +1,12 @@
-month = int(input('Please enter the current month (1-12)'))
+month = int(input('Please enter the current month (1-12): '))
 season = None
 if month == 1 or month == 2 or month == 12:
     season = 'Winter'
-elif month >= 3 and month <= 5:
+elif 3 <= month < 6:
     season = 'Spring'
-elif month >= 6 and month <= 8:
+elif 6 <= month < 9:
+    season = 'Summer'
+elif 9 <= month < 12:
     season = 'Autumn'
 else:
     season = 'Incorrect'
